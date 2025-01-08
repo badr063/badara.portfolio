95d70cc76d22f801f2699c4c0c48c9141bb0f5ff
badr063 <bbadara183@gmail.com>
2025-01-08 20:45:07 +0000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,65 @@
+/* Menu mobile */
+
+function menuMobile() {
+  const btn = document.querySelector('.burger');
+  const header = document.querySelector('.header');
+  const links = document.querySelectorAll('.navbar a');
+
+  btn.addEventListener('click', () => {
+    header.classList.toggle('show-nav');
+  });
+
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      header.classList.remove('show-nav');
+    });
+  });
+}
+menuMobile();
+
+// animation skills
+
+const observerIntersectionAnimation = () => {
+  const sections = document.querySelectorAll('section');
+  const skills = document.querySelectorAll('.skills .bar');
+
+  sections.forEach((section, index) => {
+    if (index === 0) return;
+    section.style.opacity = "0";
+    section.style.transition = "all 1.6s";
+  });
+
+  skills.forEach((elem, index) => {
+
+    elem.style.width = "0";
+    elem.style.transition = "all 1.6s";
+  });
+
+  let sectionObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.opacity = 1;
+      }
+    });
+  });
+
+  sections.forEach(section => {
+    sectionObserver.observe(section);
+  });
+
+  let skillsObserver = new IntersectionObserver(function (entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+        elem.style.width = elem.dataset.width + '%';
+      }
+    });
+  });
+
+  skills.forEach(skill => {
+    skillsObserver.observe(skill);
+  });
+}
+observerIntersectionAnimation();
+
